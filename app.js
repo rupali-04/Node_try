8b7ab32fe36486c7940ed8e78275a9d322585142
rupali-04 <rupali-04>
2020-09-09 02:59:36 +0530

    Modify app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+const express = require('express') // returns a function
+const app = express() // returns a object
+
+const workshop = [
+    {id: 1, name: 'Workshop1'},
+    {id: 2, name: 'Workshop2'},
+    {id: 3, name: 'Workshop3'}
+]
+
+app.get('/api/workshops', (req , res) => {
+    res.send(workshop)
+})
+app.get('/api/workshop1', (req , res) => {
+    res.send(workshop[0])
+})
+app.get('/api/workshop2', (req , res) => {
+    res.send(workshop[1])
+})
+app.get('/api/workshop3', (req , res) => {
+    res.send(workshop[2])
+})
+
+
+
+app.listen(3000, () => {console.log("Server running at port 3000......")} )
