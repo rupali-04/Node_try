765cc315e491c1cae049c071189c1b753938951d
rupali-04 <rupali-04>
2020-09-10 11:18:16 +0530

    modify app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express') // returns a function
 const app = express() // returns a object
 
-const workshop = [
+const workshops = [
     {id: 1, name: 'Workshop1'},
     {id: 2, name: 'Workshop2'},
     {id: 3, name: 'Workshop3'}
 ]
 
 app.get('/api/workshops', (req , res) => {
-    res.send(workshop)
+    res.send(workshops)
 })
+/*
 app.get('/api/workshop1', (req , res) => {
     res.send(workshop[0])
 })
@@ -19,7 +20,11 @@ app.get('/api/workshop2', (req , res) => {
 app.get('/api/workshop3', (req , res) => {
     res.send(workshop[2])
 })
-
-
+*/
+app.get('/api/workshops/:id', (req , res) => {
+ const workshop = workshops.find(w => w.id === parseInt(req.params.id))
+ if(!workshop) return res.status(404).send('Requested workshop not found!!')
+    res.send(workshop)
+})
 
 app.listen(3000, () => {console.log("Server running at port 3000......")} )
